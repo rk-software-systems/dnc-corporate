3bd8c12ae3c6b4fd8f72907b526ec1c7132b7246
Roman St <fcromich@gmail.com>
2024-01-23 02:28:14 +0100

    feat: fix contact us form

--- Public/DNCCorporate.Public.Web/Pages/ContactUs.cshtml.cs
+++ Public/DNCCorporate.Public.Web/Pages/ContactUs.cshtml.cs
@@ -7,72 +7,61 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Options;
 
-namespace DNCCorporate.Public.Web.Pages
+namespace DNCCorporate.Public.Web.Pages;
+
+public class ContactUsModel(IEmailSenderService emailSenderService, IOptions<BusinessSettings> businessSettingsOptions) : PageModel
 {
-    public class ContactUsModel(IEmailSenderService emailSenderService, IOptions<BusinessSettings> businessSettingsOptions) : PageModel
+    #region fields       
+
+    private readonly IEmailSenderService _emailSenderService = emailSenderService;
+    private readonly BusinessSettings _businessSettings = businessSettingsOptions.Value;
+
+    #endregion
+
+    #region properties
+
+    public ContactUsFormRequestViewModel Form { get; set; } = new ContactUsFormRequestViewModel(string.Empty, string.Empty, string.Empty, string.Empty);
+
+    #endregion
+
+
+    #region methods 
+
+    public void OnGet()
     {
-        #region fields       
-
-        private readonly IEmailSenderService _emailSenderService = emailSenderService;
-        private readonly BusinessSettings _businessSettings = businessSettingsOptions.Value;
-
-        #endregion
-
-        #region properties
-
-        public ContactUsFormRequestViewModel Form { get; set; } = new ContactUsFormRequestViewModel(string.Empty, string.Empty, string.Empty, string.Empty);
-
-        public bool? IsSuccess { get; set; }
-
-        #endregion
-
-
-        #region methods 
-
-        public void OnGet()
-        {
-        }
-
-
-        public async Task<IActionResult> OnPost(ContactUsRequestViewModel request)
-        {
-            IsSuccess = ModelState.IsValid;            
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    var sb = new StringBuilder();
-                    sb.AppendLine(CultureInfo.InvariantCulture, $"<p>Full Name: {request.Form.FullName}</p>");
-                    sb.AppendLine(CultureInfo.InvariantCulture, $"<p>Email Address: {request.Form.EmailAddress}</p>");
-                    sb.AppendLine(CultureInfo.InvariantCulture, $"<p>Subject: {request.Form.Subject}</p>");
-                    sb.AppendLine(CultureInfo.InvariantCulture, $"<p>Message: {request.Form.Message}</p>");
-                    sb.AppendLine(CultureInfo.InvariantCulture, $"<p>IP Address: {HttpContext.Connection.RemoteIpAddress}</p>");
-                    sb.AppendLine(CultureInfo.InvariantCulture, $"<p>Sent On: {DateTime.UtcNow}</p>");
-
-                    await _emailSenderService.SendEmail(new EmailMessageViewModel
-                    (
-                        $"Contact Us Form Submission - {request.Form.FullName} - {request.Form.EmailAddress}",
-                        sb.ToString(),
-                        _businessSettings.Email
-                    ));
-                }
-                catch (Exception)
-                {
-                    IsSuccess = false;
-                }
-            }
-
-            return new PartialViewResult
-            {
-                ViewName = "_ContactUsResultPartial",
-                ViewData = ViewData,
-                TempData = TempData
-            };
-        }
-        #endregion
-
-        #region helpers
-        #endregion
     }
+
+    public async Task<IActionResult> OnPost(ContactUsRequestViewModel request)
+    {
+        var isSuccess = ModelState.IsValid;            
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                var sb = new StringBuilder();
+                sb.AppendLine(CultureInfo.InvariantCulture, $"<p>Full Name: {request.Form.FullName}</p>");
+                sb.AppendLine(CultureInfo.InvariantCulture, $"<p>Email Address: {request.Form.EmailAddress}</p>");
+                sb.AppendLine(CultureInfo.InvariantCulture, $"<p>Subject: {request.Form.Subject}</p>");
+                sb.AppendLine(CultureInfo.InvariantCulture, $"<p>Message: {request.Form.Message}</p>");
+                sb.AppendLine(CultureInfo.InvariantCulture, $"<p>IP Address: {HttpContext.Connection.RemoteIpAddress}</p>");
+                sb.AppendLine(CultureInfo.InvariantCulture, $"<p>Sent On: {DateTime.UtcNow}</p>");
+
+                await _emailSenderService.SendEmail(new EmailMessageViewModel
+                (
+                    $"Contact Us Form Submission - {request.Form.FullName} - {request.Form.EmailAddress}",
+                    sb.ToString(),
+                    _businessSettings.Email
+                ));
+            }
+            catch (Exception)
+            {
+                isSuccess = false;
+            }
+        }
+
+        var result = new ContactUsResponseViewModel(isSuccess);
+        return new JsonResult(result);
+    }
+    #endregion
 }
